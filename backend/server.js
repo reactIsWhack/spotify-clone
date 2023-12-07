718bb56900cc5a43251bd6e2fd93fb5c1bd71ade
Ryan Naini <ryan.naini29@gmail.com>
2023-12-07 17:18:58 -0500

    Added start script

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
 const router = require("./routes/songRoute");
-const PORT = 5000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
