643e06410a2f8d0d06317ad141806ffc06b754e7
Ryan Naini <ryan.naini29@gmail.com>
2023-12-16 10:39:44 -0500

    Changed proxy to deployed backend url

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://lyriks-n8iv.onrender.com/",
         changeOrigin: true,
       },
     },
